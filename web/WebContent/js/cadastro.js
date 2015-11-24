a569a5717f475df7b782a1c13a85a599c59e8f10
Robson Ferreira <robeferr@gmail.com>
2015-11-24 11:59:44 -0200

    mascara cadastro

--- web/WebContent/js/cadastro.js
+++ web/WebContent/js/cadastro.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	
-	$("#cpf").mask('999.999.999-99');
-	$("#rg").mask('99.999.999-9');
-	$("#data").mask('99/99/9999');
+	$("#cpf").mask('999.999.999-99', {clearIfNotMatch: true});
+	$("#rg").mask('99.999.999-9', {clearIfNotMatch: true});
+	$("#data").mask('99/99/9999', {clearIfNotMatch: true});
+	$("#tel").mask('(99) 9999-9999', {clearIfNotMatch: true});
+	$("#cep").mask('99999-999', {clearIfNotMatch: true});
      
 });
