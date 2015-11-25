bdfcffbfbb9a842f82ab26714226080fdf6dc21f
Robson Ferreira <robeferr@gmail.com>
2015-11-24 22:52:40 -0200

    lightbox jquery

--- web/WebContent/js/lance.js
+++ web/WebContent/js/lance.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
 	
 	$("#valor").mask('#.##0,00', {reverse: true});
 	$("#data").mask('99/99/9999', {clearIfNotMatch: true});
+
+
+$('#botao1').click(function(){
+         $('.imageRow a:first').click();
+    })
      
 });
